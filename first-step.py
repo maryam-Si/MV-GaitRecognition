f59940709aa00d3528ef555a16aa09dfbd02bc90
marya-Si <maryammaryamsadeghi6@gmail.com>
2021-11-16 10:08:40 +0330

    Reduce noises by median filter and use mobile's camera for capturing frames

--- first-step.py
+++ first-step.py
@@ -1,22 +1,36 @@
 import cv2 as cv
 cap = cv.VideoCapture(0)
-#kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3,3))
-fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
+
+# using mobile's camera
+url= "http://192.168.43.1:8080/video"
+cap.open(url)
+
+# subtracting background
+
+fgbg = cv.createBackgroundSubtractorKNN()
+
+
+# other ways
+
+#fgbg = cv.bgsegm.createBackgroundSubtractorK()
 #fgbg = cv.bgsegm.BackgroundSubtractorGMG()
 #fgbg = cv.createBackgroundSubtractorMOG2(detectShadows=True)
-#fgbg = cv.createBackgroundSubtractorKNN(detectShadows=True)
+
 while True:
     ret, frame = cap.read()
     if frame is None:
         break
     fgmask = fgbg.apply(frame)
-    #fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)
+
+    # removing noise by median filter
+    filteredFrame = cv.medianBlur(fgmask,5)
 
     cv.imshow('Frame', frame)
-    cv.imshow('FG MASK Frame', fgmask)
+    cv.imshow('FG MASK Frame', filteredFrame)
 
     keyboard = cv.waitKey(30)
     if keyboard == 'q' or keyboard == 27:
         break
+    
 cap.release()
 cv.destroyAllWindows()
